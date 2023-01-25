108ead74e4970fb9e02670d8c7a125ce3f3172f5
Yoo Haechan <gocksdl7979@gmail.com>
2023-01-25 20:41:31 +0900

    feat: intialize project

---
+++ src/main/java/com/example/uzuranking/UzuRankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.uzuranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzuRankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzuRankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uzuranking/UzuRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uzuranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzuRankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
